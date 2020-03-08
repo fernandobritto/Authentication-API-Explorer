61fa1f1493e25bf5ca6d756a07582ed004e0dd5c
Fernando Britto <me@fernandobritto.com.br>
2020-03-08 07:44:54 -0300

    Initial commit

--- vendor/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://*******************', { useMongoClient: true })
-
-//Config. Padrão: classe de Promises do Mongo
-mongoose.Promise = global.Promise
-
-module.exports = mongoose
