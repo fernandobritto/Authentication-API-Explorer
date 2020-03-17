3948d15ba3fb2bf5b24607e973dc62c3638f02e5
Fernando Britto <me@fernandobritto.com.br>
2020-03-17 17:39:35 -0300

    Initial commit

--- server.js
+++
@@ -1,7 +0,0 @@
-import app from './app'
-
-const port = 8025
-app.listen(port, () => {
-  console.log()
-  console.log('')
-})
--- src/database/index.js
+++
@@ -1,9 +0,0 @@
-import Sequelize from 'sequelize'
-import databaseConfig from '../config/database'
-import User from '../models/User'
-
-const model = [User]
-
-const connection = new Sequelize(databaseConfig)
-
-model.forEach((model) => model.init(connection))
