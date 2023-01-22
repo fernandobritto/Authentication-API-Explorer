c08b0d5b52da566de92cb864ebd99d89a2d2e821
Fernando <fernandobritto.web@gmail.com>
2023-01-22 20:34:15 -0300

    Initial commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { AuthService } from './auth.service'
+import { MongooseModule } from '@nestjs/mongoose'
+import { PassportModule } from '@nestjs/passport'
+import { UsersSchema } from '../users/schemas/users.schema'
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UsersSchema,
+      },
+    ]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.TOKEN_SECRET,
+      signOptions: {
+        expiresIn: process.env.TOKEN_EXPIRATION,
+      },
+    }),
+  ],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
